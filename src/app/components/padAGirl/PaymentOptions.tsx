1ed3fb7e682012cc5e54b022e11cb992a8964500
theophilusnuer <tphilus003@gmail.com>
2025-06-27 23:09:46 +0000

    added venmo to the padher donation options

--- src/app/components/padAGirl/PaymentOptions.tsx
+++ src/app/components/padAGirl/PaymentOptions.tsx
@@ -1,11 +1,10 @@
-"use client";
+'use client';
 import Image from "next/image";
 import { useState } from "react";
 import { createPortal } from "react-dom";
 import zelle from "../../assets/images/zelle.svg";
 import cashapp from "../../assets/images/cashapp.png";
-// import zelleQR from "../../assets/images/zelleQR.jpg";
-// import cashappQR from "../../assets/images/cashappQR.png";
+import venmo from "../../assets/images/venmo.png";
 import Link from "next/link";
 
 export default function PaymentOptions({
@@ -21,7 +20,7 @@ export default function PaymentOptions({
     <>
       {createPortal(
         <div className="fixed inset-0 p-4 bg-black/55 flex items-center justify-center z-50">
-          <div className="bg-white p-4 rounded-lg max-w-md w-full relative">
+          <div className="bg-white p-2 md:p-4 rounded-lg max-w-md w-full relative">
             <button
               onClick={onClose}
               className="absolute top-2 right-3 text-black font-bold cursor-pointer"
@@ -31,12 +30,12 @@ export default function PaymentOptions({
             <h3 className="text-lg font-medium mb-4 text-center">
               Donation Options
             </h3>
-            <div className="flex mb-4">
+            <div className="flex mb-4 border-b ">
               <button
                 onClick={() => setActiveTab("zelle")}
-                className={`flex-1 py-2 flex items-center justify-center space-x-2 ${
+                className={`flex-1 md:py-2 py-1 flex items-center justify-center space-x-1 ${
                   activeTab === "zelle"
-                    ? "border-b-2 border-[#F2C94C] border-4"
+                    ? "border-b-2 border-[#F2C94C] border-4 mb-2"
                     : ""
                 }`}
               >
@@ -47,13 +46,13 @@ export default function PaymentOptions({
                   height={24}
                   className="h-7 w-7"
                 />
-                <span>Zelle</span>
+                <span className="text-sm md:text-base">Zelle</span>
               </button>
               <button
                 onClick={() => setActiveTab("cashapp")}
-                className={`flex-1 py-2 flex items-center justify-center space-x-2 ${
+                className={`flex-1 md:py-2 py-1 flex items-center justify-center space-x-1 ${
                   activeTab === "cashapp"
-                    ? "border-b-2 border-[#F2C94C] border-4"
+                    ? "border-b-2 border-[#F2C94C] border-4 mb-2"
                     : ""
                 }`}
               >
@@ -64,25 +63,30 @@ export default function PaymentOptions({
                   height={24}
                   className="h-5.5 w-5.5"
                 />
-                <span>CashApp</span>
+                <span className="text-sm md:text-base">CashApp</span>
+              </button>
+              <button
+                onClick={() => setActiveTab("venmo")}
+                className={`flex-1 md:py-2 py-1 flex items-center justify-center space-x-1 ${
+                  activeTab === "venmo"
+                    ? "border-b-2 border-[#F2C94C] border-4 mb-2"
+                    : ""
+                }`}
+              >
+                <Image
+                  src={venmo}
+                  alt="Venmo"
+                  width={24}
+                  height={24}
+                  className="h-5 w-5"
+                />
+                <span className="text-sm md:text-base">Venmo</span>
               </button>
             </div>
             <div className="text-center">
               {activeTab === "zelle" && (
                 <div>
-                  {/* <div className="">
-                    <p className="text-sm text-gray-600">Scan to Donate</p>
-                    <Image
-                      src={zelleQR}
-                      alt="Scan to pay QR code"
-                      width={115}
-                      height={115}
-                      className="mt-2 mx-auto"
-                    />
-                  </div> */}
                   <div className="mt-3">
-                    {/* <p className="text-sm text-gray-600">Or?</p> */}
-                    {/* <p className="text-sm text-gray-600">Zelle Donation Details:</p> */}
                     <p>
                       <span className="text-gray-600">Name: </span>{" "}
                       <span className="font-bold">Juliana Buah</span>
@@ -96,16 +100,6 @@ export default function PaymentOptions({
               )}
               {activeTab === "cashapp" && (
                 <div>
-                  {/* <div className="">
-                    <p className="text-sm text-gray-600">Scan to Donate</p>
-                    <Image
-                      src={cashappQR}
-                      alt="Scan to pay QR code"
-                      width={115}
-                      height={115}
-                      className="mt-2 mx-auto"
-                    />
-                  </div> */}
                   <Link href="/https://cash.app/$WAWEFregister">
                     <button className="bg-[#F2C94C] text-black py-1 px-3 md:px-6 md:py-3 rounded-md hover:scale-102 transition-all duration-200 cursor-pointer">
                       Click to Donate
@@ -118,6 +112,24 @@ export default function PaymentOptions({
                     </p>
                     <p>
                       <span className="text-gray-600">Name: </span>$WAWEF
+                    </p>
+                  </div>
+                </div>
+              )}
+              {activeTab === "venmo" && (
+                <div>
+                  <Link href="https://venmo.com/u/Juliana-Buah">
+                    <button className="bg-[#F2C94C] text-black py-1 px-3 md:px-6 md:py-3 rounded-md hover:scale-102 transition-all duration-200 cursor-pointer">
+                      Click to Donate
+                    </button>
+                  </Link>
+                  <div className="mt-3">
+                    <p className="text-sm text-gray-600">Or?</p>
+                    <p className="text-sm text-gray-600">
+                      Venmo Donation Details:
+                    </p>
+                    <p>
+                      <span className="text-gray-600">Username: </span>@juliana-Buah
                     </p>
                   </div>
                 </div>
